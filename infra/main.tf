7a644aaf7dc96e1abb594c7f6f0e279b826d9cfd
Vaibhav Malwade <developer.netcore88@gmail.com>
2026-03-31 15:42:36 +0000

    Added code to select instance type based on workspace

--- infra/main.tf
+++ infra/main.tf
@@ -15,7 +15,7 @@ provider "aws" {
 # Create a instance
 resource "aws_instance" "example" {
   ami           = "resolve:ssm:/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64"
-  instance_type = "t3.micro"
+  instance_type = terraform.workspace == "prod" ? "t3.large" : "t3.micro"
 
   tags = {
     Name = "HelloWorld"
