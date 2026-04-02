1df48acc387b6298a31ddc956130a32ea6eb0f74
Vaibhav <developer.netcore88@gmail.com>
2026-04-02 12:14:08 +0530

    Few more updates

--- init/tools/install_pre_rack.bat
+++ init/tools/install_pre_rack.bat
@@ -2,5 +2,11 @@ choco install terraform -y
 
 choco install git.install -y
 
+##choco install vscode -y
+
+choco install tflint -y
+
+choco install tfsec -y
+
 git config --global user.email "developer.netcore88@gmail.com"
 git config --global user.name "Vaibhav"
