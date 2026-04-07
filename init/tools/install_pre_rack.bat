71e6bd2d0bacef148c17bf48fecaa5d9a180a63d
Vaibhav <developer.netcore88@gmail.com>
2026-04-07 14:38:41 +0000

    Added choco command to install .net 10 version

--- init/tools/install_pre_rack.bat
+++ init/tools/install_pre_rack.bat
@@ -2,11 +2,13 @@ choco install terraform -y
 
 choco install git.install -y
 
+choco install dotnet-runtime --version=10.0.1
+
 ##choco install vscode -y
 
-choco install tflint -y
+##choco install tflint -y
 
-choco install tfsec -y
+##choco install tfsec -y
 
 git config --global user.email "developer.netcore88@gmail.com"
 git config --global user.name "Vaibhav"
