722f12a176e532dbb8c4b7043409f51470a35015
Vaibhav Malwade <developer.netcore88@gmail.com>
2026-04-07 23:34:24 +0530

    Added Hotel management lambda fucntion application

--- src/HotelMan_HotelAdmin/HotelAdmin.cs
+++ src/HotelMan_HotelAdmin/HotelAdmin.cs
@@ -1,39 +1,171 @@
-﻿using Amazon.Lambda.APIGatewayEvents;
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+using Amazon;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+using Amazon.DynamoDBv2.DocumentModel;
+using Amazon.Lambda.APIGatewayEvents;
 using Amazon.Lambda.Core;
-using System.Net.Http.Headers;
+using Amazon.Lambda.Serialization.SystemTextJson;
+using Amazon.S3;
+using Amazon.S3.Model;
+using Amazon.SimpleNotificationService;
+using Amazon.SimpleNotificationService.Model;
+using AutoMapper;
+using HotelMan_HotelAdmin.Models;
+using HttpMultipartParser;
 
-[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+[assembly: LambdaSerializer(typeof(DefaultLambdaJsonSerializer))]
 
-namespace HotelMan_HotelAdmin
+namespace HotelMan_HotelAdmin;
+
+public class HotelAdmin
 {
-    public class HotelAdmin
+    public async Task<APIGatewayProxyResponse> ListHotels(APIGatewayProxyRequest request)
     {
-        public APIGatewayProxyResponse AddHotel(APIGatewayProxyRequest request, ILambdaContext context)
+        // query string parameter called token is passed to this lambda method.
+
+        var response = new APIGatewayProxyResponse
         {
+            Headers = new Dictionary<string, string>(),
+            StatusCode = 200
+        };
 
-            //HotelMan_HotelAdmin::HotelMan_HotelAdmin::HotelAdmin::AddHotel
+        response.Headers.Add("Access-Control-Allow-Origin", "*");
+        response.Headers.Add("Access-Control-Allow-Headers", "*");
+        response.Headers.Add("Access-Control-Allow-Methods", "OPTIONS,GET");
+        response.Headers.Add("Content-Type", "application/json");
 
-            var response = new APIGatewayProxyResponse()
-            {
-                 Headers = new Dictionary<string, string>(),
-                 Body =
-                 StatusCode = 200
-            };
-
-            response.Headers.Add("Access-Control-Allow-Origin", "*");
-            response.Headers.Add("Access-Control-Allow-Headers", "*");
-            response.Headers.Add("Access-Control-Allow-Methods", "OPTIONS,POST");
-
-            Console.WriteLine("OK..");
-
+        if (request?.QueryStringParameters == null)
+        {
+            Console.WriteLine(
+                "Query string is null. You must configure the Query String Mapping in your API resource in API Gateway");
             return response;
         }
+
+        var token = request.QueryStringParameters.ContainsKey("token") ? request.QueryStringParameters["token"] : "";
+        if (string.IsNullOrEmpty(token))
+        {
+            response.StatusCode = (int)HttpStatusCode.BadRequest;
+            response.Body = JsonSerializer.Serialize(new { Error = "Query parameter 'token' not present." });
+            return response;
+        }
+
+
+        var tokenDetails = new JwtSecurityToken(token);
+        var userId = tokenDetails.Claims.FirstOrDefault(x => x.Type == "sub")?.Value;
+
+        var region = Environment.GetEnvironmentVariable("AWS_REGION");
+        var dbClient = new AmazonDynamoDBClient(RegionEndpoint.GetBySystemName(region));
+        using var dbContext = new DynamoDBContext(dbClient);
+
+        var hotels = await dbContext.ScanAsync<Hotel>(new[] { new ScanCondition("UserId", ScanOperator.Equal, userId) })
+            .GetRemainingAsync();
+
+        response.Body = JsonSerializer.Serialize(new { Hotels = hotels });
+
+        return response;
     }
 
-    public class Hotel
+    public async Task<APIGatewayProxyResponse> AddHotel(APIGatewayProxyRequest request, ILambdaContext context)
     {
-        public string? Name { get; set; }
-        public int ID { get; set; }
-        public string? Location { get; set; }
+        var response = new APIGatewayProxyResponse
+        {
+            Headers = new Dictionary<string, string>(),
+            StatusCode = 200
+        };
+
+        response.Headers.Add("Access-Control-Allow-Origin", "*");
+        response.Headers.Add("Access-Control-Allow-Headers", "*");
+        response.Headers.Add("Access-Control-Allow-Methods", "OPTIONS,POST");
+        response.Headers.Add("Content-Type", "application/json");
+
+        var bodyContent = request.IsBase64Encoded
+            ? Convert.FromBase64String(request.Body)
+            : Encoding.UTF8.GetBytes(request.Body);
+
+        Console.WriteLine($"Request size after decode: {bodyContent.Length}");
+
+        await using var memStream = new MemoryStream(bodyContent);
+        var formData = await MultipartFormDataParser.ParseAsync(memStream).ConfigureAwait(false);
+
+        var hotelName = formData.GetParameterValue("hotelName");
+        var hotelRating = formData.GetParameterValue("hotelRating");
+        var hotelCity = formData.GetParameterValue("hotelCity");
+        var hotelPrice = formData.GetParameterValue("hotelPrice");
+
+        var file = formData.Files.FirstOrDefault();
+        var fileName = file.FileName;
+
+
+        var userId = formData.GetParameterValue("userId");
+        var idToken = formData.GetParameterValue("idToken");
+
+        var token = new JwtSecurityToken(idToken);
+        var group = token.Claims.FirstOrDefault(x => x.Type == "cognito:groups");
+        if (group == null || group.Value != "Admin")
+        {
+            response.StatusCode = (int)HttpStatusCode.Unauthorized;
+            response.Body = JsonSerializer.Serialize(new { Error = "Unauthorised. Must be a member of Admin group." });
+        }
+
+        var region = Environment.GetEnvironmentVariable("AWS_REGION");
+        var bucketName = Environment.GetEnvironmentVariable("bucketName");
+
+
+        var client = new AmazonS3Client(RegionEndpoint.GetBySystemName(region));
+        var dbClient = new AmazonDynamoDBClient(RegionEndpoint.GetBySystemName(region));
+
+        try
+        {
+            await client.PutObjectAsync(new PutObjectRequest
+            {
+                BucketName = bucketName,
+                Key = fileName,
+                InputStream = file.Data
+            });
+
+            var hotel = new Hotel
+            {
+                UserId = userId,
+                Id = Guid.NewGuid().ToString(),
+                Name = hotelName,
+                CityName = hotelCity,
+                Price = int.Parse(hotelPrice),
+                Rating = int.Parse(hotelRating),
+                FileName = fileName
+            };
+
+            using var dbContext = new DynamoDBContext(dbClient);
+            await dbContext.SaveAsync(hotel);
+
+            var mapperConfig = new MapperConfiguration(cfg =>
+                cfg.CreateMap<Hotel, HotelCreatedEvent>()
+                    .ForMember(dest => dest.CreationDateTime,
+                        opt => opt.MapFrom(src => DateTime.Now))
+            );
+
+            var mapper = new Mapper(mapperConfig);
+
+            var hotelCreatedEvent = mapper.Map<Hotel, HotelCreatedEvent>(hotel);
+
+            var snsClient = new AmazonSimpleNotificationServiceClient();
+            var publishResponse = await snsClient.PublishAsync(new PublishRequest
+            {
+                TopicArn = Environment.GetEnvironmentVariable("snsTopicArn"),
+                Message = JsonSerializer.Serialize(hotelCreatedEvent)
+            });
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+
+
+        response.Body = JsonSerializer.Serialize(new { Message = "Hotel information was stored successfully." });
+        return response;
     }
 }
---
+++ src/README.md
@@ -0,0 +1,29 @@
+# Build Microservices with AWS and .NET (Second Edition)
+# The Hotel Manager Microservice
+
+
+![Microservices with .NET and AWS ](https://img-b.udemycdn.com/course/750x422/2080118_8bbf_8.jpg)
+
+
+
+This microservice code is part of an online course called ["Build Microservices with .NET and Amazon Web Services"](https://www.udemy.com/course/build-microservices-with-aspnet-core-amazon-web-services/?referralCode=B288BF33506B34292176)
+
+
+The course explains how a microservices-based system is designed and developed using Amazon Web Services and .Net.
+
+View the entire course here:
+
+["Build Microservices with .NET and Amazon Web Services"](https://www.udemy.com/course/build-microservices-with-aspnet-core-amazon-web-services/?referralCode=B288BF33506B34292176)
+
+## More Free Courses on YouTube
+
+[![YouTube](https://img.shields.io/badge/YouTube-Subscribe-red?style=flat&logo=youtube)](http://www.youtube.com/@FreeTechnologyLectures)
+
+Subscribe to the Free Technology and Technology Management Courses channel for free lectures about Coding, DevOps, and Technology Management. [Here is the link to the YouTube channel](http://www.youtube.com/@FreeTechnologyLectures).
+
+
+## Buy me a coffee ☕
+
+If you find my work helpful, consider treating me by buying me a coffee!
+
+<a href="https://ko-fi.com/arefkarimi"><img src="https://storage.ko-fi.com/cdn/kofi2.png?v=3" alt="ko-fi" height="36"></a>
