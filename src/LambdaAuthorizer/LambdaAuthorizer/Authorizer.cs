0d5834dec0dc91c6ebfdaa65419bb0b3212e938d
Vaibhav <developer.netcore88@gmail.com>
2026-05-21 07:35:24 +0000

    New lambda custom authorizer created for GetListHotel API

---
+++ src/LambdaAuthorizer/LambdaAuthorizer/Authorizer.cs
@@ -0,0 +1,95 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Text.Json;
+using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Core;
+using Amazon.SecretsManager;
+using Amazon.SecretsManager.Model;
+using Microsoft.IdentityModel.Tokens;
+
+[assembly:LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+
+namespace LambdaAuthorizer;
+
+public class Authorizer
+{
+    public async Task<APIGatewayCustomAuthorizerResponse> Auth(APIGatewayCustomAuthorizerRequest request)
+    {
+        
+        
+        var idToken = request.QueryStringParameters["token"];
+        Console.WriteLine($"Token is {idToken}");
+        var idTokenDetails = new JwtSecurityToken(idToken);
+
+        var kid = idTokenDetails.Header["kid"].ToString();
+        var issuer = idTokenDetails.Claims.First(x => x.Type == "iss").Value;
+        var audience = idTokenDetails.Claims.First(x => x.Type == "aud").Value;
+        var userId = idTokenDetails.Claims.First(x => x.Type == "sub").Value;
+        
+        var response = new APIGatewayCustomAuthorizerResponse()
+        {
+            PrincipalID = userId,
+            PolicyDocument = new APIGatewayCustomAuthorizerPolicy()
+            {
+                Version = "2012-10-17",
+                Statement = new List<APIGatewayCustomAuthorizerPolicy.IAMPolicyStatement>()
+                {
+                    new APIGatewayCustomAuthorizerPolicy.IAMPolicyStatement()
+                    {
+                        Action = new HashSet<string>(){"execute-api:Invoke"},
+                        Effect = "Allow",
+                        Resource = new HashSet<string>(){request.MethodArn}
+                    }
+                }
+            }
+        };
+        
+
+        var secretsClient = new AmazonSecretsManagerClient();
+        var secret = await secretsClient.GetSecretValueAsync(new GetSecretValueRequest
+        {
+            SecretId = "hotelCognitoKey"
+        });
+
+        var privateKeys = secret.SecretString;
+
+        Console.WriteLine($"JWKS set: {privateKeys}");
+        var jwks = new JsonWebKeySet(privateKeys);
+        
+        foreach (var key in jwks.Keys)
+        {
+            Console.WriteLine(key.Kid);
+        }
+        var privateKey = jwks.Keys.First(x => x.Kid == kid);
+
+        var handler = new JwtSecurityTokenHandler();
+        var result = await handler.ValidateTokenAsync(idToken, new TokenValidationParameters
+        {
+            ValidIssuer = issuer,
+            ValidAudience = audience,
+            IssuerSigningKey = privateKey
+        });
+
+        if (!result.IsValid) throw new UnauthorizedAccessException("Token not valid");
+
+        var apiGroupMapping = new Dictionary<string, string>()
+        {
+            {"listadminshotel+", "Admin"},
+            {"admin+", "Admin"}
+        };
+
+        var expectdGroup = apiGroupMapping.FirstOrDefault(x =>
+            request.Path.Contains(x.Key, StringComparison.InvariantCultureIgnoreCase));
+
+        if (!expectdGroup.Equals(default(KeyValuePair<string, string>)))
+        {
+            var userGroup = idTokenDetails.Claims.First(x => x.Type == "cognito:groups").Value;
+            if (string.Compare(userGroup , expectdGroup.Value, 
+                    StringComparison.InvariantCultureIgnoreCase) != 0)
+            {
+                response.PolicyDocument.Statement[0].Effect = "Deny";
+            }
+        }
+        
+        return response;
+    }
+}
---
+++ src/LambdaAuthorizer/README.md
@@ -0,0 +1,30 @@
+# Build Microservices with AWS and .NET (Second Edition)
+## AWS Customer Authorizer (formerly Lambda Authorizer) 
+
+
+![Microservices with .NET and AWS ](https://img-b.udemycdn.com/course/750x422/2080118_8bbf_8.jpg "Microservices with .NET and AWS")
+
+
+
+This HTML & Javascript website is part of an online course called ["Build Microservices with .NET and Amazon Web Services"](https://www.udemy.com/course/build-microservices-with-aspnet-core-amazon-web-services/?referralCode=B288BF33506B34292176)
+
+This website acts as the front end for a microservices-based Hotel Booking system developed during the above-mentioned course. 
+
+The course explains how a microservices-based system is developed and what relevant design patterns are applied in scenarios.
+
+View the full course here:
+
+["Build Microservices with .NET and Amazon Web Services"](https://www.udemy.com/course/build-microservices-with-aspnet-core-amazon-web-services/?referralCode=B288BF33506B34292176)
+
+## More Free Courses on YouTube
+
+[![YouTube](https://img.shields.io/badge/YouTube-Subscribe-red?style=flat&logo=youtube)](http://www.youtube.com/@FreeTechnologyLectures)
+
+Subscribe to the Free Technology and Technology Management Courses channel for free lectures about Coding, DevOps, and Technology Management. [Here is the link to the YouTube channel](http://www.youtube.com/@FreeTechnologyLectures).
+
+
+## Buy me a coffee ☕
+
+If you find my work helpful, consider treating me by buying me a coffee!
+
+<a href="https://ko-fi.com/arefkarimi"><img src="https://storage.ko-fi.com/cdn/kofi2.png?v=3" alt="ko-fi" height="36"></a>
